8ce8447079bb79d91ca21da04f9fba466be5e480
leovimartinez <leovioz@hotmail.com>
2016-08-17 21:39:50 -0500

    Se agrega el modelo salon y la relacion uno a uno

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+c2=Curso.create(nombre:"Android Free", fecha:"01-06-2016", precio:100000.00)
+c3=Curso.create(nombre:"Ruby on Rails", fecha:"18-08-2016", precio:87000.00)
+c4=Curso.create(nombre:"ExtJS", fecha:"01-09-2016", precio:90000.00)
+
+a2=Alumno.create(nombre:"Leovi Oscar Martinez Dominguez", edad:32, curso_id:c2.id)
+a3=Alumno.create(nombre:"Enrique Gomez Robles", edad:32, curso_id:c3.id)
+
+s2=Salon.create(nombre:"Tulum", curso_id:c2.id)
+s3=Salon.create(nombre:"Teotihuacan", curso_id:c3.id)
