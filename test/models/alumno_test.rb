5a86610a5f04742eaf3a83984263acfcd22e0ed1
leovimartinez <leovioz@hotmail.com>
2016-08-17 22:00:38 -0500

    Se agrega el ultimo commit

--- test/models/alumno_test.rb
+++ test/models/alumno_test.rb
@@ -4,4 +4,12 @@ class AlumnoTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+	def setup
+		@x=alumnos(:one)
+	end  
+
+  	test "guarda alumno" do
+  		alum=Alumno.new({nombre:"Pedro Hakim", edad:27, curso_id:2})
+  		assert alum.save
+	end
 end
